9d70a8f0e2176e2ab019e6b5e631423ac1311ce0
Vivian Li <vrli@uchicago.edu>
2025-04-25 14:48:30 -0700

    4th try deploy...

--- my-design-prototype/vite.config.js
+++ my-design-prototype/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/palmetto-prototype-2/', // Use the repository name as the base path
+  base: '/palmetto-prototype-2/', // Set base to repository name
 })
