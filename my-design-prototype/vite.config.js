50f022c603aca3312327139358597590ed37e0ee
Vivian Li <vrli@uchicago.edu>
2025-04-25 14:24:12 -0700

    try again

--- my-design-prototype/vite.config.js
+++ my-design-prototype/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths for GitHub Pages
+  base: '/palmetto-prototype-2/', // Use the repository name as the base path
 })
